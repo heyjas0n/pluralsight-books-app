91101599b10c33d5ff0bcf6dc7b76e642bb6435f
Jason Nguyen <jasonvex@gmail.com>
2017-10-03 23:27:19 -0500

    Adds error text view. Adds Google API key and url builder.

--- app/src/main/java/com/jason/books/BookListActivity.java
+++ app/src/main/java/com/jason/books/BookListActivity.java
@@ -4,17 +4,22 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class BookListActivity extends AppCompatActivity {
+    private ProgressBar mLoadingProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        mLoadingProgress = (ProgressBar) findViewById(R.id.pb_loading);
+
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
             new BooksQueryTask().execute(bookUrl);
@@ -40,7 +45,22 @@ public class BookListActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             TextView tvResult = (TextView) findViewById(R.id.tvResponse);
+            TextView tvError = (TextView) findViewById(R.id.tv_error);
+            mLoadingProgress.setVisibility(View.INVISIBLE);
+            if (result == null) {
+                tvResult.setVisibility(View.INVISIBLE);
+                tvError.setVisibility(View.VISIBLE);
+            } else {
+                tvResult.setVisibility(View.VISIBLE);
+                tvError.setVisibility(View.INVISIBLE);
+            }
             tvResult.setText(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
         }
     }
 }
